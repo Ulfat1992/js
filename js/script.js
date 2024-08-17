d29b7d5ead3bb995458edfbe940f721ab7e3bacd
Ulfat Jahan <167539742+Ulfat1992@users.noreply.github.com>
2024-08-17 18:20:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*ex1*/
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+
+/*ex2*/
+function getText(){
+	document.getElementById('g_text').innerHTML="Hello World";
+}
+/*ex3*/
+function myText(){
+	document.getElementById('m_text').innerHTML=" Hello I am Ulfat";
+}
+/*ex4*/
+function text(){
+	document.getElementById('text').innerHTML="Hello Bangladesh";
+}
+/*ex5*/
+function thatText(){
+	document.getElementById('t_text').innerHTML="Hello sir";
+}
